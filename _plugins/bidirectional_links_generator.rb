98f09ba5f9a36523d17f2185b7a7c43a785c1289
Shimmer93 <zpengac@connect.ust.hk>
2025-05-01 03:58:35 +0800

    update code

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -4,20 +4,28 @@ class BidirectionalLinksGenerator < Jekyll::Generator
     graph_nodes = []
     graph_edges = []
 
-    all_docs = site.posts.docs
+    all_notes = site.posts.docs
+    all_pages = site.pages
 
-    link_extension = ''
+    all_docs = all_notes + all_pages
+
+    link_extension = !!site.config["use_html_extension"] ? '.html' : ''
 
     # Convert all Wiki/Roam-style double-bracket link syntax to plain HTML
     # anchor tag elements (<a>) with "internal-link" CSS class
     all_docs.each do |current_note|
       all_docs.each do |note_potentially_linked_to|
+        note_title_regexp_pattern = Regexp.escape(
+          File.basename(
+            note_potentially_linked_to.basename,
+            File.extname(note_potentially_linked_to.basename)
+          )
+        ).gsub('\_', '[ _]').gsub('\-', '[ -]').capitalize
+
         title_from_data = note_potentially_linked_to.data['title']
         if title_from_data
           title_from_data = Regexp.escape(title_from_data)
         end
-
-        note_title_regexp_pattern = title_from_data.gsub('\_', '[ _]').gsub('\-', '[ -]').capitalize
 
         new_href = "#{site.baseurl}#{note_potentially_linked_to.url}#{link_extension}"
         anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
@@ -66,10 +74,10 @@ class BidirectionalLinksGenerator < Jekyll::Generator
     end
 
     # Identify note backlinks and add them to each note
-    all_docs.each do |current_note|
+    all_notes.each do |current_note|
       # Nodes: Jekyll
-      notes_linking_to_current_note = all_docs.filter do |e|
-        e.content.include?(current_note.url)
+      notes_linking_to_current_note = all_notes.filter do |e|
+        e.url != current_note.url && e.content.include?(current_note.url)
       end
 
       # Nodes: Graph
@@ -91,11 +99,10 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       end
     end
 
-    File.open('_includes/notes_graph.json', 'w') {|f| f.write(JSON.dump({
+    File.write('_includes/notes_graph.json', JSON.dump({
       edges: graph_edges,
       nodes: graph_nodes,
-    })) }
-
+    }))
   end
 
   def note_id_from_note(note)
